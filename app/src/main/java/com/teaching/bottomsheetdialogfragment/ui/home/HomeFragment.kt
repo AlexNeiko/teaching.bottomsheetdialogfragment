33491326c6994f0275619a9c241e49a7dbdb567d
Alik <ANeyko@synergy.ru>
2022-09-25 10:15:40 -0400

    initial data structure #BottomSheetDialogFragment with Callbacks to parent Fragment and transparent layer

---
+++ app/src/main/java/com/teaching/bottomsheetdialogfragment/ui/home/HomeFragment.kt
@@ -0,0 +1,39 @@
+package com.teaching.bottomsheetdialogfragment.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.teaching.bottomsheetdialogfragment.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+  private var _binding: FragmentHomeBinding? = null
+  private val binding get() = _binding!!
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+    _binding = FragmentHomeBinding.inflate(inflater, container, false)
+    val root: View = binding.root
+
+    val textView: TextView = binding.textHome
+    homeViewModel.text.observe(viewLifecycleOwner) {
+      textView.text = it
+    }
+    return root
+  }
+
+override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
